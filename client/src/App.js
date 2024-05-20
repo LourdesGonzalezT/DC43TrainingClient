e0f1fe7904c226296415f6f5bbcb0b21dc9c35c8
LourdesGonzalezT <LourdesGonzalezT@github.com>
2024-05-20 19:10:34 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DC43 TRAINING
+        </p>
+
+        <h1>MAS FUERTES QUE EL VINAGRE</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
